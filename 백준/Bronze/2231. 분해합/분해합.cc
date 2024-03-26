28f383d294a77228d47076fa68c2988aadea6741
Lee Jin Woo <jinwoo2511@naver.com>
2024-03-26 22:07:06 +0900

    [Bronze II] Title: 분해합, Time: 28 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Bronze/2231. 분해합/분해합.cc
@@ -0,0 +1,38 @@
+#include <iostream>
+#include <vector>
+#include <string>
+
+
+using namespace std;
+
+
+int main()
+{
+    int n = 0;
+    int m = 0;
+    int answer = 0;
+    string str;
+    cin >> m;
+
+
+
+    for (int i = 0; i < m; i++)
+    {
+        str = to_string(i);
+
+        for (int j = 0; j < str.size(); j++)
+        {
+            answer += str[j]-'0';
+        }
+
+        if (m == i + answer)
+        {
+            answer = i;
+            break;
+        }
+        else
+            answer = 0;
+    }
+
+    cout << answer << endl;
+}
