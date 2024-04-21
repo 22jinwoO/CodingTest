47f614c14d4cc667ee930f85c75a6792702c2e0f
Lee Jin Woo <jinwoo2511@naver.com>
2024-04-22 00:14:14 +0900

    [level 1] Title: 정수 내림차순으로 배치하기, Time: 0.04 ms, Memory: 4.22 MB -BaekjoonHub

--- 프로그래머스/1/12933. 정수 내림차순으로 배치하기/정수 내림차순으로 배치하기.cpp
+++ 프로그래머스/1/12933. 정수 내림차순으로 배치하기/정수 내림차순으로 배치하기.cpp
@@ -2,16 +2,38 @@
 #include <vector>
 #include <iostream>
 #include <algorithm>
+#include <iostream>
 
 using namespace std;
 
 long long solution(long long n) {
+    
     long long answer = 0;
+    
     string ns = to_string(n);
+    
+    cout << ns << endl;
+    
     int idx = 0;
 
-    sort(ns.begin(), ns.end(),greater<>());
+    // 버블 정렬
+    for(int i = 0; i <ns.size()-1; i++)
+    {
+        for(int j = 0; j < ns.size()-1-i; j++)
+        {
+            if(ns[j]<ns[j+1])
+            {
+                int temp = 0;
+                temp = ns[j+1];
+                ns[j+1] = ns[j];
+                ns[j] = temp;
+            }
+        }
+    }
+
     answer=stoll(ns); 
+    
+    cout<<answer<<endl;
     
     return answer;
 
