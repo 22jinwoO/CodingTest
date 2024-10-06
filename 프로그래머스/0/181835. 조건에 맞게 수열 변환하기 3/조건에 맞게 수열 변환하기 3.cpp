f4b63f2866fbb4fd6d6f89d3fd2d88c8595a344a
Lee Jin Woo <jinwoo2511@naver.com>
2024-10-07 08:34:16 +0900

    [level 0] Title: 조건에 맞게 수열 변환하기 3, Time: 333.25 ms, Memory: 224 MB -BaekjoonHub

---
+++ 프로그래머스/0/181835. 조건에 맞게 수열 변환하기 3/조건에 맞게 수열 변환하기 3.cpp
@@ -0,0 +1,29 @@
+#include <string>
+#include <vector>
+
+using namespace std;
+
+vector<int> solution(vector<int> arr, int k) {
+    
+    if(k % 2 != 0)
+    {
+        for(int i = 0; i < arr.size(); i++)
+        {
+            arr[i]*=k;
+        }
+    }
+    
+    else 
+    {
+        for(int i = 0; i < arr.size(); i++)
+        {
+            arr[i]+=k;
+        }
+    }
+    
+    vector<int> answer;
+    
+    answer = arr;
+    
+    return answer;
+}
