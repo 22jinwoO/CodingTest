71bc264a149921e3564b3f45ed6df596cea01abe
Lee Jin Woo <jinwoo2511@naver.com>
2024-11-29 08:08:45 +0900

    [Bronze IV] Title: 별 찍기 - 3, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Bronze/2440. 별 찍기 － 3/별 찍기 － 3.cc
@@ -0,0 +1,13 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+int main() {
+    int num;
+    cin >> num;
+    for(int i = 0; i < num; i++)
+    {
+        for(int j = 0; j < num - i; j++) cout << "*";
+        cout << '\n';
+    }
+}
