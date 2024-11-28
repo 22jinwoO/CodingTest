834823db001c29a453a852ecd1d51165b5da67ed
Lee Jin Woo <jinwoo2511@naver.com>
2024-11-29 07:46:04 +0900

    [Bronze IV] Title: 별 찍기 - 2, Time: 348 ms, Memory: 6576 KB -BaekjoonHub

---
+++ 백준/Bronze/2439. 별 찍기 － 2/별 찍기 － 2.cs
@@ -0,0 +1,18 @@
+using System;
+using System.Collections.Generic;
+
+namespace MyCompiler {
+    class Program {
+        public static void Main(string[] args) {
+            int num = int.Parse(Console.ReadLine());
+
+            for(int i = 0; i < num; i++)
+            {
+                int j = 0;
+                for(; j < num - 1 - i; j++) Console.Write(" ");
+                for(; j < num; j++) Console.Write("*");
+                Console.WriteLine("");
+            }
+        }
+    }
+}
