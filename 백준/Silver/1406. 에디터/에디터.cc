d84bde4ad62dfcdaf732eb5f00bce9aab06dd723
Lee Jin Woo <jinwoo2511@naver.com>
2024-12-11 08:37:00 +0900

    [Silver II] Title: 에디터, Time: 136 ms, Memory: 20880 KB -BaekjoonHub

--- 백준/Silver/1406. 에디터/에디터.cc
+++ 백준/Silver/1406. 에디터/에디터.cc
@@ -3,6 +3,7 @@
 using namespace std;
 
 int main() {
+
     string str;
 
     cin >> str;
@@ -15,37 +16,39 @@ int main() {
 
     // 명령어 입력 횟수
     int n;
+    
     cin >> n;
 
     // n 만큼 반복
-    for (int i = 0; i < n; i++) {
+    for(int i = 0; i < n; i ++)
+    {
         // 명령어 문자 입력
         char command;
-        cin >> command;
 
-        switch (command) {
+        cin >> command;
+        
+        switch (command) 
+        {
             // 커서 위치 왼쪽으로 이동
             case 'L':
-                // 커서가 text.begin()이 아니면 왼쪽으로 이동
-                if (cursor != text.begin()) {
+                if(cursor != text.begin())
                     cursor--;
-                }
                 break;
 
             // 커서 위치 오른쪽으로 이동
             case 'D':
-                // 커서가 text.end()가 아니면 오른쪽으로 이동
-                if (cursor != text.end()) {
+                if(cursor != text.end())
                     cursor++;
-                }
                 break;
 
             // 커서 왼쪽의 문자 삭제
             case 'B':
-                // 커서가 text.begin()이 아니면 왼쪽으로 이동 후 삭제
-                if (cursor != text.begin()) {
-                    cursor = text.erase(--cursor); // 삭제 후 커서 위치 업데이트
+                if(cursor != text.begin())
+                {
+                    cursor--;
+                    cursor = text.erase(cursor);    
                 }
+                    
                 break;
 
             // 커서 위치에 문자 추가
@@ -57,10 +60,7 @@ int main() {
         }
     }
 
-    // 최종 문자열 출력
-    for (char c : text) {
-        cout << c;
-    }
+    for(char c : text) cout << c;
 
     return 0;
 }
